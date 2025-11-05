b2f1d7e79aa7bd6eaf29d12f4c7156a6073e428d
Nicolas Perret <nicolas.perret@le-campus-numerique.fr>
2025-11-05 16:57:33 +0100

    Base code

---
+++ service-client/src/main/java/com/microcommerce/model/Client.java
@@ -0,0 +1,116 @@
+package com.microcommerce.model;
+
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "clients")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String motDePasse;
+
+    private String nom;
+    private String prenom;
+    private Integer age;
+
+    // Constructeurs
+    public Client() {
+    }
+
+    public Client(Long id, String email, String motDePasse, String nom, String prenom, Integer age) {
+        this.id = id;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.age = age;
+    }
+
+    // Getters et Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    // equals, hashCode et toString
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id) &&
+                Objects.equals(email, client.email) &&
+                Objects.equals(motDePasse, client.motDePasse) &&
+                Objects.equals(nom, client.nom) &&
+                Objects.equals(prenom, client.prenom) &&
+                Objects.equals(age, client.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, motDePasse, nom, prenom, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", motDePasse='" + motDePasse + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
