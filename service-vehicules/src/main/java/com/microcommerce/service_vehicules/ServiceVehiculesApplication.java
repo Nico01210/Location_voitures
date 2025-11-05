9fc142b844b64c6668f023caca7c33e2eea76773
Nico <nicolas.perret@le-campus-numerique.fr>
2025-11-05 15:04:38 +0100

    Add files via upload

---
+++ service-vehicules/src/main/java/com/microcommerce/service_vehicules/ServiceVehiculesApplication.java
@@ -0,0 +1,13 @@
+package com.microcommerce.service_vehicules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceVehiculesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceVehiculesApplication.class, args);
+	}
+
+}
---
+++ service-vehicules/src/test/java/com/microcommerce/service_vehicules/ServiceVehiculesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcommerce.service_vehicules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceVehiculesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
