9819a510a1736ea313a84f1b4614919762f89bcb
Nico <nicolas.perret@le-campus-numerique.fr>
2025-11-05 15:03:40 +0100

    Add files via upload

---
+++ service-reservation/src/test/java/com/microcommerce/service_reservation/ServiceReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microcommerce.service_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
