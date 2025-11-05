efeab29347e269129ef34d1f3ba461a79c649bdb
Nicolas Perret <nicolas.perret@le-campus-numerique.fr>
2025-11-05 15:32:44 +0100

    Service-client ready to code

--- service-client/src/test/java/com/microcommerce/service_client/ServiceClientApplicationTests.java
+++ service-client/src/test/java/com/microcommerce/service_client/ServiceClientApplicationTests.java
@@ -2,8 +2,10 @@ package com.microcommerce.service_client;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServiceClientApplicationTests {
 
 	@Test
