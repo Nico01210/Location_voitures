bce23ce784a823c73e3f02376ad00d788a80f4df
Nancia Laudino <nancialaudino@gmail.com>
2025-11-06 16:23:04 +0100

    Configuration of the vehicles microservice: H2 database + controllers + cleanup of .idea files

--- service-vehicules/src/main/java/com/microcommerce/service_vehicules/model/Vehicle.java
+++ service-vehicules/src/main/java/com/microcommerce/service_vehicules/model/Vehicle.java
@@ -1,4 +1,80 @@
 package com.microcommerce.service_vehicules.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-public class Vehicle {
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+public abstract class Vehicle {
+
+    @Id
+    private String registration;
+    private String brand;
+    private String model;
+    private String color;
+    private double basePrice;
+    private double pricePerKm;
+    private int fiscalPower;
+
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getBasePrice() {
+        return basePrice;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getPricePerKm() {
+        return pricePerKm;
+    }
+
+    public int getFiscalPower() {
+        return fiscalPower;
+    }
+
+    public void setRegistration(String registration) {
+        this.registration = registration;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setBasePrice(double basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setPricePerKm(double pricePerKm) {
+        this.pricePerKm = pricePerKm;
+    }
+
+    public void setFiscalPower(int fiscalPower) {
+        this.fiscalPower = fiscalPower;
+    }
+
 }
