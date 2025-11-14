f42573a865d9478de032167c2b30b48772e0b771
Nico <nicolas.perret@le-campus-numerique.fr>
2025-11-14 09:43:49 +0100

    Add files via upload

---
+++ service-front-web/src/main/java/com/locationvoiture/front/front_web/FrontWebApplication.java
@@ -0,0 +1,13 @@
+package com.locationvoiture.front.front_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontWebApplication.class, args);
+	}
+
+}
---
+++ service-front-web/src/test/java/com/locationvoiture/front/front_web/FrontWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locationvoiture.front.front_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
